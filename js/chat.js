cdf620de9fb0db594e2a560f68467b39fa39e216
himanshujaju <himanshu.jaju@gmail.com>
2013-08-25 11:47:33 +0530

    Create chat.js

---
+++ js/chat.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$("a#SEND").click(function(){
+		var e=document.getElementById("newMSG");
+		var msg=e.value;
+		if(msg.length==0)
+			alert("Enter something to chat about! Don't be shy!");
+		else
+		{
+		e.value="";
+		$("#chatMSG").append("<div class='message btn btn-info'><b> ME : </b>"+msg+"</div>");
+		$("#chatMSG").append("<div class='message bot btn btn-info'><b> BOT : </b>"+msg+"</div>");
+		}
+	});
+});
+/*
+Alternatively you can do an ajax call to a new php page sending the 'msg' via ajax. Save the message to db. On success, do the append.
+For checking new messages, keep doing an ajax call every 2 seconds to check if the database has any new message!
+If you would like to open source a database version, you can push your code to my repo or create your own repo and ping me the link!
+*/
